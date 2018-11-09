50e26298dc77604fcb33b1d61abecbbf2aaa80d7
Mirage Rattani <m.rattani0905@gmail.com>
2018-11-09 15:31:56 -0500

    Pushing through changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,18 @@
 console.log("game.js working")
+
+//computer randomly picks a letter 
+
+// user has to guess which letter
+
+// records amount of times user wins
+
+// records amount of times user loses
+
+// user guesses left which updates after each guess
+
+// user guesses so far, updated after each guess
+
+// if player wins increases win column and starts game over without refreshing page
+
+// if playedr loses increases loss column and starts game over without refreshing page
+
